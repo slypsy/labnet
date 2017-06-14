0dced3fcacf5f450f581dd14110f1dfff0b01156
slypsy <tamarakovacev13@gmail.com>
2017-06-14 17:01:25 +0200

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var htmlImport = require('gulp-html-import');
+//var clean = require('gulp-clean');
+var browserSync = require('browser-sync').create();
+const autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('sass', function () {
+    return gulp.src('./sass/**/*.scss')
+        .pipe(sass({
+            //outputStyle: 'compressed',
+            includePaths: [
+                'node_modules/susy/sass',
+                'node_modules/breakpoint-sass/stylesheets'
+            ]
+        }).on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('./dist/css'))
+        .pipe(browserSync.stream());
+});
+
+// gulp.task('clean-css', function () {
+//     return gulp.src(['./css','./dist/css'], {read: false}).pipe(clean());
+// });
+
+gulp.task('html-import', function () {
+    return gulp.src('./src/*.html')
+        .pipe(htmlImport('./src/components/'))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['html-import', 'sass'], function() {
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    });
+
+    gulp.watch('./sass/**/*.scss', [/*'clean-css',*/'sass']);
+    gulp.watch('./src/**/*.html', ['html-import']);
+    gulp.watch('./dist/**/*.html', browserSync.reload);
+});
+
